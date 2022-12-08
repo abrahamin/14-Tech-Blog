4718f0f5d3296d3f4ef62096f1f06d3e295bf405
abrahamin <abrahamin.html@gmail.com>
2022-12-08 18:40:23 -0500

    route files

---
+++ controllers/api/blogRoutes.js
@@ -0,0 +1,53 @@
+const router = require("express").Router();
+const { Post } = require("../../models/");
+const withAuth = require("../../utils/auth");
+
+router.post("/", withAuth, async (req, res) => {
+  try {
+    const newBlog = await Blog.create({
+      ...req.body,
+      user_id: req.session.user_id,
+    });
+    res.status(200).json(newBlog);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.post("/:id", withAuth, async (req, res) => {
+  try {
+    const [affectedRows] = await Blog.update(req.body, {
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (affectedRows > 0) {
+      res.status(200).end();
+    } else {
+      res.status(404).end();
+    }
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.delete("/:id", withAuth, async (req, res) => {
+  try {
+    const [affectedRows] = Post.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (affectedRows > 0) {
+      res.status(200).end();
+    } else {
+      res.status(404).end();
+    }
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
